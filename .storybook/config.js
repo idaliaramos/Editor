623a293e765f57183e0029f33de8c3bd3e28ae94
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-21 18:16:41 -0700

    cleaned up structure and files

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { configure } from "@storybook/react";
 function loadStories() {
   require("../src/stories");
   require("../src/index.css");
-    require('../src/index.js');
+  require("../src/index.js");
 }
 
 configure(loadStories, module);
--- src/components/SignInComponent.stories.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from '@storybook/addon-actions';
-import SignInComponent from "./SignInComponent";
-
-storiesOf("SignInComponent", module).add("render input layout", () => (
-  <SignInComponent />
-));
