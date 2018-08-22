e68e00bc2ef943d8726a1117f441833b6b2178a6
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-21 19:22:24 -0700

    saving the name in local storage

--- src/components/SignInComponent.js
+++ src/components/SignInComponent.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 
-const SignInComponent = () => {
-  const _onHandleSubmitName = event => {
-    event.preventDefault();
-
-    console.log("Hello", event.target);
+export default class SignInComponent extends Component {
+  state = {
+    name: ""
   };
-  return (
-    <Segment inverted>
-      <Form inverted onClick={_onHandleSubmitName}>
-        <Form.Group widths="equal">
-          <Form.Input fluid label="First name" placeholder="First name" />
-        </Form.Group>
+  onSubmitName = event => {
+    event.preventDefault();
+    this.setState({ name: event.target.value });
+    localStorage.setItem("name", this.state.name);
+  };
+  render() {
+    console.log(this.state, "state");
+    return (
+      <Segment inverted>
+        <Form inverted onChange={this.onSubmitName}>
+          <Form.Group widths="equal">
+            <Form.Input fluid label="First name" placeholder="First name" />
+          </Form.Group>
 
-        <Button type="submit">Submit</Button>
-      </Form>
-    </Segment>
-  );
-};
-export default SignInComponent;
+          <Button type="submit">Submit</Button>
+        </Form>
+      </Segment>
+    );
+  }
+}
