31eb6371a13c03f754a12c84388a7bffe26b2630
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-23 17:38:32 -0700

    storybook documentItem added

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import SignInComponent from "../components/SignInComponent";
+import DocumentItem from '../components/DocumentItem'
 
 storiesOf("SignInComponent", module).add("render input layout", () => (
   <SignInComponent />
+));
+storiesOf("DocumentItem", module).add("renders the single document", () => (
+  <DocumentItem />
 ));
