98b46d08de44a3bf3c235998da5ec94c04ee4021
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-23 17:52:24 -0700

    added stories of doc item

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import SignInComponent from "../components/SignInComponent";
-import DocumentItem from '../components/DocumentItem'
+import DocumentItem from "../components/DocumentItem";
 
 storiesOf("SignInComponent", module).add("render input layout", () => (
   <SignInComponent />
