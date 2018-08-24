298355cd733b9784f0397c62de9e5d38d4a7899c
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-24 11:37:02 -0700

    fixed the flex-box styling to be responsive

--- src/components/DocumentItem.jsx
+++ src/components/DocumentItem.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { List } from "semantic-ui-react";
+import { List, Segment } from "semantic-ui-react";
 
 const DocumentItem = () => (
-  <List divided relaxed>
-    <List.Item>
-      <List.Icon name="github" size="large" verticalAlign="middle" />
-      <List.Content>
-        <List.Header as="a">Semantic-Org/Semantic-UI</List.Header>
-        <List.Description as="a">Updated 10 mins ago</List.Description>
-      </List.Content>
-    </List.Item>
-  </List>
+  <Segment>
+    <List divided relaxed>
+      <List.Item>
+        <List.Icon name="github" size="large" verticalAlign="middle" />
+        <List.Content>
+          <List.Header as="a">Semantic-Org/Semantic-UI</List.Header>
+          <List.Description as="a">Updated 10 mins ago</List.Description>
+        </List.Content>
+      </List.Item>
+    </List>
+  </Segment>
 );
 
 export default DocumentItem;
--- src/components/EditorPageLayout.jsx
+++ src/components/EditorPageLayout.jsx
@@ -6,7 +6,7 @@ export default class EditorPageLayout extends Component {
     return (
       <div className="EditorPageLayout">
         <div className="EditorPageLayout-sideBar">{children[0]}</div>
-        {children[1]}
+        <div className="Editor">{children[1]}</div>
       </div>
     );
   }
