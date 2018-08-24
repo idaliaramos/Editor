789ada56bc1a7f51bc0e47eae69657819aee003d
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-23 19:57:45 -0700

    created story for editor component

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,10 +3,14 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import SignInComponent from "../components/SignInComponent";
 import DocumentItem from "../components/DocumentItem";
+import EditorComponent from "../components/EditorComponent";
 
 storiesOf("SignInComponent", module).add("render input layout", () => (
   <SignInComponent />
 ));
 storiesOf("DocumentItem", module).add("renders the single document", () => (
   <DocumentItem />
+));
+storiesOf("Editor", module).add("renders the editor", () => (
+  <EditorComponent />
 ));
