34376312ec08623ff8b3caac20c8ca0bc6cdbec0
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-23 18:40:54 -0700

    added story for listcomponent, and state to test items rendering

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SignInComponent from "./components/SignInComponent";
+import DocumentListComponent from "./components/DocumentListComponent";
 
 class App extends Component {
+  state = {
+    documents: {
+      doc1: {
+        owners: ["<user>", "<user>"],
+        lastChangeBy: " <user>",
+        content: "<content>"
+      },
+      doc2: {
+        owners: ["idalia", "michael"],
+        lastChangeBy: "brady",
+        content: "hello i am content"
+      }
+    }
+  };
   render() {
+    console.log(this.state.documents)
     return (
       <div className="App">
         <SignInComponent />
+        <DocumentListComponent documents={this.state.documents} />
       </div>
     );
   }
--- src/components/DocumentListComponent.jsx
+++ src/components/DocumentListComponent.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
-import DocumentItem from './components/DocumentItem'
+import DocumentItem from "./DocumentItem";
 //this renders all of the documents
 export default class DocumentList extends Component {
+  static defaultProps = {
+    documents: {}
+    // onSelect: () => {}
+  };
   render() {
-    //documents will be passed in , for each document it will create a
+    //deconstruct the docs off props
+    const { documents } = this.props;
+
+    let documentArray = [];
+    let transform = Object.entries(documents).forEach(([documentName, value]) =>
+      documentArray.push({ documentName: value })
+    );
+
+    //documents will be passed in as props, for each document it will create a
     //document Item
-    return  this.props.documents.map(document => (
-      <DocumentItem />
-    ))
+    return (
+      <div>
+        {documentArray.length === 0 ? (
+          <p className="">No notes availabe at this time.</p>
+        ) : (
+          documentArray.map(document => <DocumentItem document={document}/>)
+        )}
+      </div>
+    );
   }
 }
