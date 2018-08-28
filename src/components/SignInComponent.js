94d8a1aa08830cbcdbbc7bff20521ec396c0ed37
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-28 13:48:14 -0700

    added container with instructions

--- src/components/SignInComponent.js
+++ src/components/SignInComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Form, Segment } from "semantic-ui-react";
+import { Button, Form, Segment, Container, Header } from "semantic-ui-react";
 //SignInComponent takes in the name of the user and saves it to localStorage
 export default class SignInComponent extends Component {
   //onSubmitName sets the name to local storage
@@ -14,15 +14,20 @@ export default class SignInComponent extends Component {
   //renders a form with a single input field for the name
   render() {
     return (
-      <Segment inverted color="blue">
-        <Form inverted onChange={this.onSetName} onSubmit={this.goToEditor}>
-          <Form.Group widths="equal">
-            <Form.Input fluid label="First name" placeholder="First name" />
-          </Form.Group>
+      <div className="Sign-In-Component">
+        <Container inverted>
+          <Header as="h2">Sign in to Edit Documents</Header>
+          <Segment inverted color="blue">
+            <Form inverted onChange={this.onSetName} onSubmit={this.goToEditor}>
+              <Form.Group widths="equal">
+                <Form.Input fluid label="First name" placeholder="First name" />
+              </Form.Group>
 
-          <Button type="submit">Submit</Button>
-        </Form>
-      </Segment>
+              <Button type="submit">Submit</Button>
+            </Form>
+          </Segment>
+        </Container>
+      </div>
     );
   }
 }
