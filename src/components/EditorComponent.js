156de36a412d9ef68b5162c538cb949bc023ac5e
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-29 11:16:47 -0700

    removed old function, subbed in async func witherror handling

--- src/components/EditorComponent.js
+++ src/components/EditorComponent.js
@@ -83,25 +83,7 @@ export default class EditorComponent extends Component {
       </div>
     );
   }
-  //handle submit will take the information and
-  // _handleSubmit = event => {
-  //   event.preventDefault();
-  //   let user = localStorage.getItem("name");
-  //   let content = this.state.content;
-  //   let docInfo = {
-  //     issuer: user,
-  //     content: content
-  //   };
-  //   return fetch(`https://aachallengeone.now.sh/update/${this.state.name}`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     },
-  //     body: JSON.stringify(docInfo)
-  //   }).then(response => response.json()).catch(err => {
-  //   this.setState({isSuccessful: false})
-  //   });
-  // };
+
 
   _handleSubmit = async event => {
     event.preventDefault();
